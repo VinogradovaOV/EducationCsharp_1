c8a73ce5978c4fdca11329f72fc4d2a235e57ee6
VinogradovaOV <ka_2197@mail.ru>
2018-10-16 10:06:52 +0300

    Домашнее задание 8

---
+++ WFSample8/WFSample8/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFSample8
+{
+    /// <summary>
+    /// Форма создания вопросника
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        // База данных с вопросами
+        TrueFalse database;
+
+        #region Кнопки  и numericUpDown
+
+        // Обработчик события изменения значения numericUpDown
+        private void nudNumber_ValueChanged(object sender, EventArgs e)
+        {
+            tboxQuestion.Text = database[(int)nudNumber.Value - 1].Text;
+            cboxTrue.Checked = database[(int)nudNumber.Value - 1].TrueFalse;
+        }
+
+        // Обработчик кнопки Добавить
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (database == null)
+            {
+                MessageBox.Show("Создайте новую базу данных", "Сообщение");
+                return;
+            }
+            database.Add((database.Count + 1).ToString(), true);
+            nudNumber.Maximum = database.Count;
+            nudNumber.Value = database.Count;
+        }
+
+        // Обработчик кнопки Удалить
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (nudNumber.Maximum == 1 || database == null) return;
+            database.Remove((int)nudNumber.Value);
+            nudNumber.Maximum--;
+            if (nudNumber.Value > 1) nudNumber.Value = nudNumber.Value;
+        }      
+
+        // Обработчик кнопки Сохранить (вопрос)
+        private void btnSaveQuest_Click(object sender, EventArgs e)
+        {
+            database[(int)nudNumber.Value - 1].Text = tboxQuestion.Text;
+            database[(int)nudNumber.Value - 1].TrueFalse = cboxTrue.Checked;
+        }
+        #endregion
+
+        #region Меню
+
+        // Обработчик пункта меню Новый
+        private void miNew_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                database = new TrueFalse(sfd.FileName);
+                database.Add("123", true);
+                database.Save();
+                nudNumber.Minimum = 1;
+                nudNumber.Maximum = 1;
+                nudNumber.Value = 1;
+            };
+        }
+
+        // Обработчик пункта меню Открыть
+        private void miOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                database = new TrueFalse(ofd.FileName);
+                database.Load();
+                nudNumber.Minimum = 1;
+                nudNumber.Maximum = database.Count;
+                nudNumber.Value = 1;
+                tboxQuestion.Text = database[(int)nudNumber.Value - 1].Text;
+            }
+        }
+
+        // Обработчик пункта меню Сохранить
+        private void miSave_Click(object sender, EventArgs e)
+        {
+            if (database != null) database.Save();
+            else MessageBox.Show("База данных не создана");
+        }
+
+        //Обработчик меню Сохранить как
+        private void miSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd1 = new SaveFileDialog();
+            if (sfd1.ShowDialog() == DialogResult.OK)
+            {
+                database.FileName = sfd1.FileName;
+                if (database != null) database.Save();
+                else MessageBox.Show("База данных не создана");
+            }
+        }
+
+        //Обработчик пункта меню О программе
+        private void miProgram_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Разработчик GeekBrains совместно с Виноградовой О.В.");
+        }
+
+        //Обработчик пункта меню Выход
+        private void miExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+    }
+}
+
---
+++ WFSample8/WFSample8/Question.cs
@@ -0,0 +1,77 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+namespace WFSample8
+{
+    /// <summary>
+    ///   Класс для вопросов и ответов 
+    /// </summary>
+    [Serializable]
+    public class Question
+    {
+        private string text;       // Текст вопроса
+        private bool trueFalse;// Правда или нет
+
+        public string Text { get => text; set => text = value; }
+        public bool TrueFalse { get => trueFalse; set => trueFalse = value; }
+
+        public Question()
+        {
+        }
+
+        public Question(string text, bool trueFalse)
+        {
+            this.text = text;
+            this.trueFalse = trueFalse;
+        }
+
+        #region Блок создания xml файла Вопросов из txt
+
+        /// <summary>
+        /// Считывание из txt и сохранение в Xml
+        /// </summary>
+        static public void QuestionSave()
+        {
+            string[] str = File.ReadAllLines(@"question.txt", Encoding.GetEncoding(1251));
+            
+            List<Question> list = new List<Question>();
+
+            Regex regY = new Regex(@"[(]Да[)]$");
+            Regex regN = new Regex(@"[(]Нет[)]$");
+
+            for (int j = 0; j < str.Length; j++)
+            {
+                if (regY.Match(str[j]).Length != 0)
+                    list.Add(new Question(str[j].Remove(regY.Match(str[j]).Index,
+                        regY.Match(str[j]).Length), true));
+                if (regN.Match(str[j]).Length != 0)
+                    list.Add(new Question(str[j].Remove(regN.Match(str[j]).Index,
+                        regN.Match(str[j]).Length), false));
+            }
+
+            Save(list);
+            
+        }
+
+        /// <summary>
+        /// сохранение в Xml
+        /// </summary>
+        /// <param name="list">Список вопросов</param>
+        public static void Save(List<Question> list)
+        {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Question>));
+            Stream fStream = new FileStream("Questins.xml", FileMode.Create, FileAccess.Write);
+            xmlFormat.Serialize(fStream, list);
+            fStream.Close();
+        }
+
+        #endregion
+
+    }
+
+}
+
